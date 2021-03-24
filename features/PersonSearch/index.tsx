22bdb825ba1e4bb078eab07f06491ee3a583ec4a
Tae Kim <taekimjr@gmail.com>
2021-03-24 16:34:48 -0500

    feat: throttle loading more results

--- features/PersonSearch/index.tsx
+++ features/PersonSearch/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FuzzySearch from "fuzzy-search";
+import throttle from "lodash.throttle";
 
 import InfiniteScroller from "shared/InfiniteScroller";
 
@@ -8,8 +9,11 @@ import SearchInput from "./SearchInput";
 import PersonResult from "./PersonResult";
 import PERSON_DATA from "./person-data";
 
-// Number of results to paginate by (e.g. page 1 has 20 results)
+// Number of results to paginate by (e.g. page 1 has 20 results).
 const PAGE_SIZE = 20;
+
+// The throttled duration for loading more pages.
+const LOAD_MORE_DELAY_IN_MS = 750;
 
 const PersonSearch = () => {
   const [searchValue, setSearchValue] = React.useState("");
@@ -19,6 +23,13 @@ const PersonSearch = () => {
     new FuzzySearch(PERSON_DATA, ["name"], {
       caseSensitive: false,
     })
+  );
+
+  const throttledLoadMore = React.useCallback(
+    throttle(() => {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }, LOAD_MORE_DELAY_IN_MS),
+    []
   );
 
   const results = PersonSearcher.search(searchValue);
@@ -41,9 +52,7 @@ const PersonSearch = () => {
       <InfiniteScroller.Container
         hasMoreItems={hasMoreResults}
         loadingItems={false}
-        onLoadMore={() => {
-          setCurrentPage((prevPage) => prevPage + 1);
-        }}
+        onLoadMore={throttledLoadMore}
       >
         {shownResults.map(({ id, name, avatar, description }) => (
           <InfiniteScroller.Item key={id} showHideThreshold={2500}>
